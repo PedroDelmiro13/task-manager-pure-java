d406040e301089b491f096e36fbeba448879d283
PedroDelmiro13 <pdelmiro1309@gmail.com>
2026-01-07 20:22:36 -0300

    feat: create the base server

---
+++ src/main/java/com/pedrodelmiro/taskmanager/Main.java
@@ -0,0 +1,42 @@
+package com.pedrodelmiro.taskmanager;
+
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.Properties;
+
+/**
+ *  http server
+ */
+
+public class Main {
+    public static void main (String[] args) throws IOException{
+        Properties props = new Properties();
+        try(InputStream is =
+                Main.class.getClassLoader().getResourceAsStream("application.properties")){
+
+            if (is == null) {
+                throw new RuntimeException("application.properties not found");
+            }
+            props.load(is);
+        }
+
+        int port = Integer.parseInt(props.getProperty("server.port"));
+        String host = props.getProperty("server.host");
+        String response = "Hello World";
+
+        InetSocketAddress address = new InetSocketAddress(host, port);
+        HttpServer server = HttpServer.create(address, 0);
+
+        server.createContext("/", (exchange->{
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        }));
+
+        server.start();
+        System.out.println("server starting on http://" + host +":" + port + "/");
+    }
+}
